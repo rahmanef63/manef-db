6d0f1f36feac8b0dba145e81f1617ba431ab4d61
Rahman <rahmanef63@gmail.com>
2026-03-10 17:22:25 +0000

    fix(proxy): set upstream host + TLS SNI for Convex Cloud

--- docker/start.sh
+++ docker/start.sh
@@ -4,7 +4,13 @@ set -eu
 : "${UPSTREAM_CONVEX_URL:?UPSTREAM_CONVEX_URL is required}"
 : "${PUBLIC_DB_DOMAIN:=dbgg.rahmanef.com}"
 
-envsubst '${UPSTREAM_CONVEX_URL} ${PUBLIC_DB_DOMAIN}' \
+# Extract host from https://<host>/... for SNI + Host header to upstream
+UPSTREAM_CONVEX_HOST="$(printf '%s' "$UPSTREAM_CONVEX_URL" | sed -E 's#^[a-zA-Z]+://([^/:]+).*$#\1#')"
+: "${UPSTREAM_CONVEX_HOST:?failed to parse UPSTREAM_CONVEX_HOST}"
+
+export UPSTREAM_CONVEX_HOST
+
+envsubst '${UPSTREAM_CONVEX_URL} ${PUBLIC_DB_DOMAIN} ${UPSTREAM_CONVEX_HOST}' \
   < /etc/nginx/templates/default.conf.template \
   > /etc/nginx/conf.d/default.conf
 
